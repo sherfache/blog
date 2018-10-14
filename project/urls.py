ea85084648d7ed7e05db73f77ef2577f6abb6fc2
littlewulu <917177485@qq.com>
2018-10-14 22:38:07 +0800

    修复bug

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from app.urls import blog_patterns
-from app import localtest
 
 urlpatterns = [
     path("blog/", include(blog_patterns)),
