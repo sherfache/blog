41a98f322c7fb1c239d58dc383f0b03da95bc8b0
littlewulu <917177485@qq.com>
2018-10-18 17:10:50 +0800

    消息解析

--- weixin/index.py
+++ weixin/index.py
@@ -4,7 +4,7 @@ index 视图
 from app.views import returnOk, returnBadRequest, checkPara, returnForbidden
 from django.http import request
 from django.http import HttpResponse
-from weixin.utils import checkSignature
+from weixin.utils import checkSignature, WeixinParser
 from app.util import FileLogger
 
 # 推送入口
@@ -19,15 +19,18 @@ def index(request):
     if not checkSignature(signature, timestamp, nonce):
         return returnForbidden("Fuck off.")
 
-    FileLogger.log_info("weixin_data", request.GET, handler_name=FileLogger.WEIXIN_HANDLER)
+    FileLogger.log_info("weixin_GET_data", request.GET, handler_name=FileLogger.WEIXIN_HANDLER)
 
     # 如果是校验请求
     if request.method == "GET":
         echostr = request.GET.get("echostr")
         return HttpResponse(echostr)
 
+    msgData = WeixinParser.parseXml(request.body)
+
     # 记录文件日志
-    FileLogger.log_info("weixin_data", request.POST, handler_name=FileLogger.WEIXIN_HANDLER)
+    FileLogger.log_info("weixin_POST_data", request.POST, handler_name=FileLogger.WEIXIN_HANDLER)
+    FileLogger.log_info("weixin_data_map", msgData.__dict__, handler_name=FileLogger.WEIXIN_HANDLER)
 
     return returnOk()
 
--- weixin/utils.py
+++ weixin/utils.py
@@ -3,7 +3,7 @@
 """
 from django.conf import settings
 import hashlib
-
+from xml.etree import ElementTree
 
 # 微信签名验证方法
 def checkSignature(signature, timestamp, nonce):
@@ -11,9 +11,53 @@ def checkSignature(signature, timestamp, nonce):
     tmpArr = [token, timestamp, nonce]
     tmpArr.sort()
     strTmp = tmpArr[0] + tmpArr[1] + tmpArr[2]
-    sigLoc = hashlib.sha1(strTmp).hexdigest()
+    sigLoc = hashlib.sha1(strTmp.encode("utf8")).hexdigest()
     if sigLoc == signature:
         return True
     return False
+
+
+# 解析微信xml消息
+class WeixinParser:
+
+    @classmethod
+    def parseXml(cls, data):
+        if len(data) == 0:
+            return None
+        xmlData = ElementTree.fromstring(data)
+        msgType = xmlData.find("MsgType").text
+        if msgType == "event":
+            return cls.EventMsg(xmlData)
+        elif msgType == "text":
+            return cls.TextMsg(xmlData)
+        else:
+            return cls.Msg(xmlData)
+
+
+    class Msg(object):
+        def __init__(self, xmlData):
+            self.toUserName = xmlData.find("ToUserName").text
+            self.fromUserName = xmlData.find("FromUserName").text
+            self.createTime = xmlData.find("CreateTime").text
+            self.msgType = xmlData.find("MsgType").text
+
+    class EventMsg(Msg):
+
+        def __init__(self, xmlData):
+            super().__init__(xmlData)
+            self.event = xmlData.find("Event").text
+
+    class TextMsg(Msg):
+
+        def __init__(self, xmlData):
+            super().__init__(xmlData)
+            self.content = xmlData.find("Content").text.encode("utf-8")
+            self.msgId = xmlData.find("MsgId").text
+
+
+
+
+
+
 
 
